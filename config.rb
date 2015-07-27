1fd0a8aea8c5c9ccba70ffc40603c19448f1b493
Sean Humeniuk <seanhumeniuk@gmail.com>
2015-07-26 21:53:06 -0400

    Fix middleman deploy config

--- config.rb
+++ config.rb
@@ -71,9 +71,9 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
 
-  activate :deploy do |deploy|
-    deploy.method = :git
-    deploy.branch = 'master'
-  end
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'master'
 end
